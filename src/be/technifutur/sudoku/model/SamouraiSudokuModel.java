a3e8803a0879bc3729e2e39af71a644a99e19d09
Debroux <71833601+Cedricdebroux@users.noreply.github.com>
2022-06-03 16:12:03 +0200

    exception samourai ok

--- src/be/technifutur/sudoku/model/SamouraiSudokuModel.java
+++ src/be/technifutur/sudoku/model/SamouraiSudokuModel.java
@@ -3,8 +3,8 @@ package be.technifutur.sudoku.model;
 import be.technifutur.sudoku.controler.SudokuModel;
 
 public class SamouraiSudokuModel implements SudokuModel {
-    char [][]tab = new char [21][21];
-    public static final char EMPTY_VALUE = 0 ;
+    char[][] tab = new char[21][21];
+    public static final char EMPTY_VALUE = 0;
     public static final char IS_NOTVALID_VALUE = 'E';
 
     public int getLineSize() {
@@ -15,34 +15,47 @@ public class SamouraiSudokuModel implements SudokuModel {
         return 21;
     }
 
-    protected boolean isValidValue(char n){
-        return n > '0' && n < (9 +'0');
-    };
+    protected boolean isValidValue(char n) {
+        return n > '0' && n <= (9 + '0');
+    }
+
+    ;
+
+    public final void setValue(int line, int column, char value) throws OutOfSudokuException, InvalidValueSudokuException {
+        if (!isValid(line, column)) {
+            throw new OutOfSudokuException(line, column, "Votre position n'est pas dans le sudoku");
+        } else if (!isValidValue(value)) {
+            throw new InvalidValueSudokuException(value, "veuillez choisir une autre valeur !!!!! comprise entre : 1 et " + line);
+        }
+        tab[line][column] = value;
+    }
 
     public SamouraiSudokuModel() {
-        for (int longueur = 0; longueur < 6; longueur++){
-            for (int larg = 0; larg < 3; larg++){
-                tab[0+longueur][9+larg] = IS_NOTVALID_VALUE;
-                tab[9+larg][0+longueur] = IS_NOTVALID_VALUE;
-                tab[15+longueur][9+larg] = IS_NOTVALID_VALUE;
-                tab[9+larg][15+longueur] = IS_NOTVALID_VALUE;
+        for (int longueur = 0; longueur < 6; longueur++) {
+            for (int larg = 0; larg < 3; larg++) {
+                tab[0 + longueur][9 + larg] = IS_NOTVALID_VALUE;
+                tab[9 + larg][0 + longueur] = IS_NOTVALID_VALUE;
+                tab[15 + longueur][9 + larg] = IS_NOTVALID_VALUE;
+                tab[9 + larg][15 + longueur] = IS_NOTVALID_VALUE;
             }
         }
     }
 
     public boolean isEmpty(int line, int column) {
-        if (tab[line][column] == EMPTY_VALUE || tab[line][column] == IS_NOTVALID_VALUE){
+        if (tab[line][column] == EMPTY_VALUE || tab[line][column] == IS_NOTVALID_VALUE) {
             return true;
         }
         return false;
     }
+
     public boolean isValid(int line, int column) {
-        return tab[line][column] != IS_NOTVALID_VALUE;
+        return line >= 0
+                && line < 21
+                && column >= 0
+                && column < 21
+                && tab[line][column] != IS_NOTVALID_VALUE;
     }
 
-    public void setValue(int line, int column, char value) {
-        tab [line][column] = value;
-    }
 
     public char getValue(int line, int column) {
         return tab[line][column];
